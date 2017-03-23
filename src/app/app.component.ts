a59c4ac4a501fcbb6deaa7538ed3c83fd51adbc8
JHades <jhades.dev@gmail.com>
2017-03-23 22:31:49 +0100

    fix issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'hello-world',
-  template: '<app-search-box [placeholder]="Custom Placeholder"></app-search-box>',
+  template: `<app-search-box [placeholder]="'Custom Placeholder'"></app-search-box>`,
   styleUrls: ['./app.component.css'] 
 })
 export class HelloWorld {
